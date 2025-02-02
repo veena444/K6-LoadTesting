224e20477e0a47c0eaca43b5112cbce4c25d6ec9
Veena Hegde <veena444@gmail.com>
2025-02-02 18:40:55 +0530

    Added reading the payload from external file

--- POST-APItest.js
+++ POST-APItest.js
@@ -7,18 +7,15 @@ export const options = {
 
 }
 
-const url = "https://reqres.in/api/users"
+const url = "https://reqres.in/api/users";
 
-const payload = {
-    "name": "Veena",
-    "job": "Tester"
-}
+const data = open('./payload.json')
+const payload = JSON.stringify(JSON.parse(data))
 
 export default function(){
-   const response =  http.post(url,payload)
-
-   console.log("*** printing payload ***", payload)
-   console.log("*** printing response ***",response.body)
+   const response =  http.post(url, payload);
+   console.log("*** printing payload ***", payload);
+   console.log("*** printing response ***",response.body);
 
     check(response,{'status code validation':(response) => response.status == 201,
         'Response ID validation':(response) => response.body.includes('id')
